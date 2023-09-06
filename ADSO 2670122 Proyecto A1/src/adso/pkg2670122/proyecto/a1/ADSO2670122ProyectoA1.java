3daade70ac16631b8b5826d6811f752923ace623
NotMe <notMe@not.com>
2023-09-05 19:02:10 -0500

    Proyecto A1

---
+++ ADSO 2670122 Proyecto A1/src/adso/pkg2670122/proyecto/a1/ADSO2670122ProyectoA1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package adso.pkg2670122.proyecto.a1;
+
+/**
+ *
+ * @author SENA
+ */
+public class ADSO2670122ProyectoA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
